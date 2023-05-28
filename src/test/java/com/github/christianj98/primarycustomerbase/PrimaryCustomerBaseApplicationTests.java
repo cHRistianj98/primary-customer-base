a5baead4e3b83553a50af8b54144ff49a2c22f7f
vanvi <kania.praca@wp.pl>
2023-05-28 10:12:12 +0200

    Initial commit

---
+++ src/test/java/com/github/christianj98/primarycustomerbase/PrimaryCustomerBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.christianj98.primarycustomerbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryCustomerBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
