5693b7b59ef93a6532519b8c89172dd1f835a50c
vanvi <kania.praca@wp.pl>
2023-05-30 21:20:21 +0200

    Add E2E test for customer

---
+++ src/test/java/com/github/christianj98/primarycustomerbase/controller/CustomerControllerE2ETest.java
@@ -0,0 +1,51 @@
+package com.github.christianj98.primarycustomerbase.controller;
+
+import com.github.christianj98.primarycustomerbase.dto.CustomerDto;
+import com.github.christianj98.primarycustomerbase.entity.Customer;
+import com.github.christianj98.primarycustomerbase.repository.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class CustomerControllerE2ETest {
+    private static final String FIRST_NAME = "Jan";
+    private static final String LAST_NAME = "Kowalski";
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private CustomerDto customerDto;
+
+    @BeforeEach
+    public void init() {
+        customerDto = createCustomerDto();
+    }
+
+    @Test
+    public void createCustomer_ResourceAlreadyExists_ExceptionThrown() {
+        // given
+        restTemplate.postForEntity("/customers", customerDto, Void.class);
+
+        // when
+        ResponseEntity<Void> response = restTemplate.postForEntity("/customers", customerDto, Void.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+    }
+
+    private CustomerDto createCustomerDto() {
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setFirstName(FIRST_NAME);
+        customerDto.setLastName(LAST_NAME);
+        return customerDto;
+    }
+}
