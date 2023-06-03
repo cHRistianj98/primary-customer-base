d1a0863fafd1e7cea565c7c21f7a84c172d7bf72
vanvi <kania.praca@wp.pl>
2023-06-03 10:43:27 +0200

    Add rest of data jpa tests

--- src/test/java/com/github/christianj98/primarycustomerbase/repository/CustomerRepositoryTest.java
+++ src/test/java/com/github/christianj98/primarycustomerbase/repository/CustomerRepositoryTest.java
@@ -1,13 +1,13 @@
 package com.github.christianj98.primarycustomerbase.repository;
 
 import com.github.christianj98.primarycustomerbase.entity.Customer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.github.christianj98.primarycustomerbase.utils.CustomerTestUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,24 +15,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 public class CustomerRepositoryTest {
     @Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
     private CustomerRepository customerRepository;
 
-    @Test
-    @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-    public void whenFindAll_ThenReturnAllUsers() {
-        // given
-        Customer customer = createCustomer(FIRST_NAME, LAST_NAME);
-        entityManager.merge(customer);
+    private Customer customer;
 
+    @BeforeEach
+    public void init() {
+        customer = customerRepository.save(createCustomer(FIRST_NAME, LAST_NAME));
+    }
+
+    @Test
+    public void whenFindAll_ThenReturnAllUsers() {
         // when
         final List<Customer> customers = customerRepository.findAll();
 
         // then
         assertThat(customers).hasSize(1);
-        assertThat(customers).containsOnly(customer);
+        assertThat(customers).extracting(Customer::getFirstName).containsOnly(FIRST_NAME);
+        assertThat(customers).extracting(Customer::getLastName).containsOnly(LAST_NAME);
     }
 
+    @Test
+    public void whenSaveCustomer_ThenReturnCustomer() {
+        // when
+        final Customer createdCustomer = customerRepository.save(customer);
+
+        // then
+        assertThat(createdCustomer).isNotNull();
+        assertThat(createdCustomer.getFirstName()).isEqualTo(FIRST_NAME);
+        assertThat(createdCustomer.getLastName()).isEqualTo(LAST_NAME);
+    }
+
+    @Test
+    public void whenFindById_ThenReturnOptionalCustomer() {
+        // when
+        final Optional<Customer> foundCustomer = customerRepository.findById(customer.getId());
+
+        // then
+        assertThat(foundCustomer).isPresent();
+        assertThat(foundCustomer.get().getFirstName()).isEqualTo(FIRST_NAME);
+        assertThat(foundCustomer.get().getLastName()).isEqualTo(LAST_NAME);
+    }
+
+    @Test
+    public void whenExistsById_ThenReturnTrue() {
+        // when
+        final boolean customerExists = customerRepository.existsById(customer.getId());
+
+        // then
+        assertThat(customerExists).isTrue();
+    }
+
+    @Test
+    public void whenExistsByFirstNameAndLastName_ThenReturnTrue() {
+        // when
+        final boolean customerExists = customerRepository.existsByFirstNameAndLastName(FIRST_NAME, LAST_NAME);
+
+        // then
+        assertThat(customerExists).isTrue();
+    }
 }
