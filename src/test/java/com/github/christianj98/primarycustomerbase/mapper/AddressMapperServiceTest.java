a0b2f365a5d285afe5eb2ee7c867388b00851fe1
vanvi <kania.praca@wp.pl>
2023-06-06 23:37:51 +0200

    Create unit tests for AddressMapperService

---
+++ src/test/java/com/github/christianj98/primarycustomerbase/mapper/AddressMapperServiceTest.java
@@ -0,0 +1,53 @@
+package com.github.christianj98.primarycustomerbase.mapper;
+
+import com.github.christianj98.primarycustomerbase.dto.AddressDto;
+import com.github.christianj98.primarycustomerbase.entity.Address;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.github.christianj98.primarycustomerbase.utils.AddressTestUtils.CITY;
+import static com.github.christianj98.primarycustomerbase.utils.AddressTestUtils.STREET;
+import static com.github.christianj98.primarycustomerbase.utils.AddressTestUtils.createAddress;
+import static com.github.christianj98.primarycustomerbase.utils.AddressTestUtils.createAddressDto;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for {@link AddressMapperService}
+ */
+@ExtendWith(MockitoExtension.class)
+public class AddressMapperServiceTest {
+
+    @InjectMocks
+    private AddressMapperService addressMapperService;
+
+    @Test
+    @DisplayName("Map address entity from address dto")
+    public void shouldMapFromAddressDto() {
+        // given
+        final AddressDto addressDto = createAddressDto(STREET, CITY);
+
+        // when
+        final Address address = addressMapperService.mapFrom(addressDto);
+
+        // then
+        assertThat(address.getStreet()).isEqualTo(addressDto.getStreet());
+        assertThat(address.getCity()).isEqualTo(addressDto.getCity());
+    }
+
+    @Test
+    @DisplayName("Map address dto from address entity")
+    public void shouldMapFromAddress() {
+        // given
+        final Address address = createAddress(STREET, CITY);
+
+        // when
+        final AddressDto addressDto = addressMapperService.mapFrom(address);
+
+        // then
+        assertThat(addressDto.getStreet()).isEqualTo(address.getStreet());
+        assertThat(addressDto.getCity()).isEqualTo(address.getCity());
+    }
+}
